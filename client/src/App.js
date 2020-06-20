3ea6ddac1bf7f1aed118bb67b2828bec954a90c5
Utry Máté <36498701+utrymate@users.noreply.github.com>
2020-06-20 21:38:04 +0200

    Add files via upload

--- client/src/App.js
+++ client/src/App.js
@@ -1,26 +1,21 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import ProductStore from './ProductStore'
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import {PrivateRoute} from "./components/PrivateRoute";
+import MainLogin from "./MainLogin";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+    return (
+        <BrowserRouter>
+            <Switch >
+                <Route exact path='/' component={MainLogin}/>
+                <Route exact path='/login' component={MainLogin}/>
+                <PrivateRoute exact path='/app' component={ProductStore}/>
+            </Switch>
+        </BrowserRouter>
+    );
 }
 
 export default App;
